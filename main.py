dd601e05dc2c76007fa384c41075d6435e67176c
Danika170 <131435599+Danika170@users.noreply.github.com>
2023-04-21 10:08:48 +0200

    Update main.py
    danokokenyesi@gmail.com

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import pandas as pd
 
 if __name__ == '__main__':
     diamonds = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/diamonds.csv')
-    ##print(diamonds)
 
-    ##print(diamonds['carat'])
-
+    print(diamonds['carat'])
     sum = 0
+
     for i in diamonds['carat']:
         sum += i
-    print('karát összesen: ' + str(sum))
-
+    print(sum)
     darabszam = diamonds['carat'].count()
-    print('darabszám: '+ str(darabszam))
-
-
     atlag = sum / darabszam
     print('átlag: ' + str(atlag))
+
+    for gyemant in diamonds.iterrows():
+        color = gyemant[1]['color']
+        if color == 'H':
+            print(gyemant)
